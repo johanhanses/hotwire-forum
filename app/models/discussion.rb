57c2d940abdfb1a8c71cb133bb155c268192f63c
Johan Hanses <johanhanses@gmail.com>
2021-07-16 20:23:18 +0200

    Add new post when creating a discussion

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,12 +1,13 @@
 class Discussion < ApplicationRecord
   belongs_to :user, default: -> { Current.user }
+  has_many :posts, dependent: :destroy
+  accepts_nested_attributes_for :posts
 
   validates :name, presence: true
 
   after_create_commit -> { broadcast_prepend_to "discussions" }
   after_update_commit -> { broadcast_replace_to "discussions" }
   after_destroy_commit -> { broadcast_remove_to "discussions" }
-
 
   def to_param
     "#{id}-#{name.downcase.to_s[0...100]}".parameterize
