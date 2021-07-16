2061c9e64c117bd61bbc9ab66ced6ffc5bbbff6c
Johan Hanses <johanhanses@gmail.com>
2021-07-16 07:12:59 +0200

    Edit discussions from UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails .application .routes .draw do
     devise_for :users
 
-    resources :discussions, only: [:index, :create, :new]
+    resources :discussions, only: [:index, :new, :create, :edit, :update, :destroy]
 
     root to: 'main#index'
   end
